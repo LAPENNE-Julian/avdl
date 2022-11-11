91d1d133ca25af5cb4483d2be3f73df947e6439f
LAPENNE-Julian <lapenne.julian@gmail.com>
2022-11-11 18:00:30 +0100

    Clean Css responsive for error 404 page

--- src/Templates/error/err403.tpl.php
+++ src/Templates/error/err403.tpl.php
@@ -1,5 +1,7 @@
-<h1>403 Forbidden</h1>
+<section id="erro403" class="container-fluid errorPage">
+    <h1>403 Forbidden</h1>
 
     <p>no access</p>
 
     <a href="/">back to home</a>
+</section>
--- src/Templates/error/err404.tpl.php
+++ src/Templates/error/err404.tpl.php
@@ -1,5 +1,21 @@
-<h1>404 Not Found</h1>
+<section id="error404" class="container-fluid errorPage">
+    <div id="error404-inner">
+        
+        <h1>Oups !</h1>
+        
+        <div id="error404-item-content">
+            
+            <img src="<?php $_SERVER['HTTP_HOST'] ?>/assets/css/images/404.jpg" alt="404.jpg">
+           
+            <div id="error404-item-data">
+                
+                <p>La page que vous recherchez semble introuvable.</p>
 
-    <p>not found</p>
+                <p>Code erreur : <span class="label-bold">404</span></p>
 
-    <a href="/">Back to home</a>
+                <p><a href="/">Retourner a la page d'Acceuil</a></p>
+
+            </div>
+        </div>
+    </div>
+</section>
