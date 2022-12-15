eb3e3c8d727866d457ba0a6ecab3f5b1a48e228d
LAPENNE-Julian <lapenne.julian@gmail.com>
2022-12-15 18:20:38 +0100

    Modify directory

--- src/Templates/layout/header.tpl.php
+++ src/Templates/layout/header.tpl.php
@@ -14,8 +14,8 @@
     <!--bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php $_SERVER['HTTP_HOST'] ?>/assets/css/reset.css">
-    <link rel="stylesheet" href="<?php $_SERVER['HTTP_HOST'] ?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?php $_SERVER['HTTP_HOST'] ?>/css/reset.css">
+    <link rel="stylesheet" href="<?php $_SERVER['HTTP_HOST'] ?>/css/style.css">
 </head>
 
 <body>
